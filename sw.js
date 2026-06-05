678782ad930de21bc7af3a49b3f030a7687dd9d9
John <jcr0916@github>
2026-06-05 22:31:26 +0000

    sw: bump cache to fr-v3

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    On first visit: cache everything. On repeat visits: instant load.
    On offline: serve cached version. */
 
-const CACHE = 'fr-v2';
+const CACHE = 'fr-v3';
 
 const PAGES = [
   '/', '/index.html',
