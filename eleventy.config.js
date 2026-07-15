b15b635efde62b9bd7d73b00a33348cc8b12ff14
jcr0916-hue <jcr0916@gmail.com>
2026-07-15 07:35:22 -0700

    proof and news

---
+++ eleventy.config.js
@@ -0,0 +1,7 @@
+export default function (eleventyConfig) {
+  return {
+    dir: { input: "src", output: "_site", includes: "_includes" },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+}
